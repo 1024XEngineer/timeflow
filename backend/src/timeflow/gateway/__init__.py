fc2a00ac8673a684e17cd9897461a1fa981b69e2
Rick Guo <hello@liuxi.ng>
2026-07-30 10:56:34 +0800

    Revert "feat(backend): add voice parsing pipeline (#101)"
    This reverts commit a720cd07a3952b2e2f2a5da9f03e95a10efd8146.

--- backend/src/timeflow/gateway/__init__.py
+++ backend/src/timeflow/gateway/__init__.py
@@ -1,13 +1,1 @@
-"""Gateway adapters for third-party services."""
-
-from timeflow.gateway.aliyun_asr import AliyunASRClient, AliyunASRClientError, AliyunASRSettings
-from timeflow.gateway.openai_llm import OpenAILLMClient, OpenAIResponseError, OpenAISettings
-
-__all__ = [
-    "AliyunASRClient",
-    "AliyunASRClientError",
-    "AliyunASRSettings",
-    "OpenAILLMClient",
-    "OpenAIResponseError",
-    "OpenAISettings",
-]
+"""Outbound adapters for external providers."""
